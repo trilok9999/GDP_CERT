288660aed87710692387bf0a2623ee0cf5b985e2
s521942 <kumar.challa7893@gmail.com>
2015-10-08 22:23:07 -0500

    Added location

--- Client/js/cert.js
+++ Client/js/cert.js
@@ -1,5 +1,4 @@
-var myApp = angular.module('myApp',['ngMaterial', 'ngAria', 'ngAnimate','ngFileUpload','uiGmapgoogle-maps']);
-
+var myApp = angular.module('myApp',['ngMaterial', 'ngAria', 'ngAnimate','ngFileUpload','uiGmapgoogle-maps', 'ngAutocomplete']);
 
 myApp.config(function($mdThemingProvider) {
   $mdThemingProvider.theme('default')
@@ -74,7 +73,9 @@ function certController($timeout, $q, $scope, $rootScope, $mdSidenav, $mdDialog,
   $rootScope.members = [];
   $scope.user={};
   $rootScope.mdMedia=$mdMedia;
-
+    $rootScope.result1 = '';
+    $rootScope.options1 = null;
+    $rootScope.details1 = '';
   $scope.setGrpMembers = function(group){
     var dupeUserIndexes = [];
     for(var j=0;j<group.members.length;j++){
@@ -801,3 +802,66 @@ myApp.factory('socket', function ($rootScope) {
     }
   };
 });
+
+angular.module( "ngAutocomplete", []).directive('ngAutocomplete', function($parse) {
+        return {
+
+            scope: {
+                details: '=',
+                ngAutocomplete: '=',
+                options: '='
+            },
+
+            link: function(scope, element, attrs, model) {
+
+                //options for autocomplete
+                var opts
+
+                //convert options provided to opts
+                var initOpts = function() {
+                    opts = {}
+                    if (scope.options) {
+                        if (scope.options.types) {
+                            opts.types = []
+                            opts.types.push(scope.options.types)
+                        }
+                        if (scope.options.bounds) {
+                            opts.bounds = scope.options.bounds
+                        }
+                        if (scope.options.country) {
+                            opts.componentRestrictions = {
+                                country: scope.options.country
+                            }
+                        }
+                    }
+                }
+                initOpts()
+
+                //create new autocomplete
+                //reinitializes on every change of the options provided
+                var newAutocomplete = function() {
+                    scope.gPlace = new google.maps.places.Autocomplete(element[0], opts);
+                    google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
+                        scope.$apply(function() {
+//              if (scope.details) {
+                            scope.details = scope.gPlace.getPlace();
+//              }
+                            scope.ngAutocomplete = element.val();
+                        });
+                    })
+                }
+                newAutocomplete()
+
+                //watch options provided to directive
+                scope.watchOptions = function () {
+                    return scope.options
+                };
+                scope.$watch(scope.watchOptions, function () {
+                    initOpts()
+                    newAutocomplete()
+                    element[0].value = '';
+                    scope.ngAutocomplete = element.val();
+                }, true);
+            }
+        };
+    });
