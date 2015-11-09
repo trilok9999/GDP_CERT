d9fdbe6318663206acc1fc9e942e3e84e65545ac
kilarusravankumar <kilarusravankumar@hotmailcom>
2015-11-09 00:56:36 -0500

    legend added in cert.js, maps html and cert css

--- Client/js/cert.js
+++ Client/js/cert.js
@@ -59,6 +59,7 @@ myApp.controller('certController',certController);
 
 
 
+
 function certController($timeout, $q, $scope, $rootScope, $mdSidenav, $mdDialog, $http, Upload, $filter, $mdMedia, socket,verifyDelete,Success) {
  
   $scope.flag = "home";
@@ -943,3 +944,67 @@ angular.module( "ngAutocomplete", []).directive('ngAutocomplete', function($pars
             }
         };
     });
+
+
+
+ myApp.controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+        $scope.toggleLeft = buildDelayedToggler('left');
+        $scope.toggleRight = buildToggler('right');
+        $scope.isOpenRight = function(){
+            return $mdSidenav('right').isOpen();
+        };
+
+        /**
+         * Supplies a function that will continue to operate until the
+         * time is up.
+         */
+        function debounce(func, wait, context) {
+            var timer;
+
+            return function debounced() {
+                var context = $scope,
+                    args = Array.prototype.slice.call(arguments);
+                $timeout.cancel(timer);
+                timer = $timeout(function() {
+                    timer = undefined;
+                    func.apply(context, args);
+                }, wait || 10);
+            };
+        }
+
+        /**
+         * Build handler to open/close a SideNav; when animation finishes
+         * report completion in console
+         */
+        function buildDelayedToggler(navID) {
+            return debounce(function() {
+                $mdSidenav(navID)
+                    .toggle()
+                    .then(function () {
+                        $log.debug("toggle " + navID + " is done");
+                    });
+            }, 200);
+        }
+
+        function buildToggler(navID) {
+            return function() {
+                $mdSidenav(navID)
+                    .toggle()
+                    .then(function () {
+                        $log.debug("toggle " + navID + " is done");
+                    });
+            }
+        }
+    })
+
+    .controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+        $scope.close = function () {
+            $mdSidenav('right').close()
+                .then(function () {
+                    $log.debug("close RIGHT is done");
+                });
+        };
+    });
+
+
+
